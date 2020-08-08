e57587add1b39f5b45839300e28b908e9155b016
kamwanasamwel <kamwanasamwel@gmail.com>
2020-08-08 19:02:51 +0300

    adding cards display component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 
+// import Cards from "./components/Cards/Cards";
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { Cards, Chart, CountryPicker } from "./components";
+
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
   render() {
     return (
-      <div>
-        <h1>Hello, am working</h1>
+      // const { data } = this.state;
+
+      <div className={styles.container}>
+        <Cards data={this.state.data} />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }
