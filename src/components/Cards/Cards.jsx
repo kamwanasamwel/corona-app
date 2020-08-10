685ed1407d667d51ecec00e7aa0e0611afc5d20d
kamwanasamwel <kamwanasamwel@gmail.com>
2020-08-10 14:43:07 +0300

    resolved render bug

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,6 +6,7 @@ import {
   Grid,
   StylesProvider,
 } from "@material-ui/core";
+import CountUp from "react-countup";
 
 import styles from "./Cards.module.css";
 
@@ -18,7 +19,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdated } }) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography variant="h5">{confirmed && confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
